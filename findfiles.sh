f200548a8511048d552eba640fe8e8d1d71591dc
Brandon Powell <bjpowel2@oakland.edu>
2013-05-20 11:44:52 -0400

    Now outputs the contents of $dir.

--- findfiles.sh
+++ findfiles.sh
@@ -9,4 +9,7 @@ fi
 filename=$1
 dir=$2
 echo "Searching directory: $dir"
+echo "$dir contents:"
+ls $dir
+echo
 egrep -h -o "[a-Z_]*(\.php|\.pl)" $filename | sort | uniq | awk '{cmd="[ -f " "'$dir'" $1 " ]";if(system(cmd)==0){print "File " $1 " found in ""'$dir'";}}' 
